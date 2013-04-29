12b917e51473740994d6bc784fc1365a916a4438
Rhys Powell <rhys@rpowell.me>
2013-04-29 10:26:10 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deconfound::Application.config.secret_key_base = 'e87c16f7c99e7e9df80caf3ac09daf9c492ea0801851327e05a8a2d55d70844549a1b97b7fdf588d8695100edf19372713d9149307e147b55b8df0c7f5298135'
