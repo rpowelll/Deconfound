478680545073d93f6baa0484d3292dd239f4f104
Rhys Powell <rhys@rpowell.me>
2013-04-29 11:12:07 +1000

    Performed pending database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130429002613) do
+ActiveRecord::Schema.define(version: 20130429011032) do
+
+  create_table "answers", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", force: true do |t|
     t.string   "title"
