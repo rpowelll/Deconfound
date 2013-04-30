91589117847c6ca1d7492a17b28e5f7fb0bdb7a8
Rhys Powell <rhys@rpowell.me>
2013-05-01 09:26:18 +1000

    Require a UOW email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ActiveRecord::Base
   validates :username,  presence: true, 
                         uniqueness: true
 
-  validates :email,     presence: true, 
-                        uniqueness: true
-                        
-  validates :password,  presence: true, 
+  validates :email,     presence: true,
+                        uniqueness: true,
+                        format: { with: /[a-z]{2,3}\d{3}@uowmail\.edu\.au/, 
+                                  message: "We're only accepting UOW students for now" }
+
+  validates :password,  presence: true,
                         confirmation: true
 
   has_many :questions
