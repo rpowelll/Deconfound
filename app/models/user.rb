b435380c1a14edd52a9da2a80ef73984fb79b964
Rhys Powell <rhys@rpowell.me>
2013-05-01 09:22:08 +1000

    Quick refactor of user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true
+  validates :username,  presence: true, 
+                        uniqueness: true
+
+  validates :email,     presence: true, 
+                        uniqueness: true
+                        
+  validates :password,  presence: true, 
+                        confirmation: true
 
   has_many :questions
   has_many :answers
