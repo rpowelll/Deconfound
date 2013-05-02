5eb85781b978c12ef74ccd10544b7392144f22e5
Rhys Powell <rhys@rpowell.me>
2013-05-02 13:47:36 +1000

    Only show vote links if the user hasn't voted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
 		markdown.render(string).html_safe
 	end
+
+	def current_user_has_voted_on?(votable)
+		votable.votes.where({user: current_user}).count > 0
+	end
 end
